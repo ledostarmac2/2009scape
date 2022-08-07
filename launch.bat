9daf2d2e248822c8583a9f23dc89ee712da271fb
Ceikry <ironceikry@gmail.com>
2022-08-07 18:20:55 +0000

    Clear anything already passed into _JAVA_OPTIONS so users can't blame us for breaking their own environment.

--- launch.bat
+++ launch.bat
@@ -1,6 +1,8 @@
 @echo off
 title 2009scape console log
 
+set _JAVA_OPTIONS=
+
 netstat -an | findstr "0.0.0.0:43595" > NUL
 if %ERRORLEVEL% equ 0 ( 
   echo "Port 43595 is in use. Cannot start the server again."
