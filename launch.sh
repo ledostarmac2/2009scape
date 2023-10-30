fca9b9d351130ceda830b1ffb6f861785984957e
dginovker <danielginovker@gmail.com>
2023-10-30 19:11:49 +0900

    Allow passing path to custom Java executable

--- launch.sh
+++ launch.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Usage: ./script.sh [path_to_java]
+# If path_to_java is not provided, the script will use the default java command.
+
 export _JAVA_OPTIONS=
 SCRIPT_DIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 FIRST_RUN=1
+
+# Use the provided java command or fall back to the default
+JAVA_CMD=${1:-java}
 
 lsof -i:43595
 
@@ -21,7 +27,7 @@ else
 fi
 
 cd $SCRIPT_DIR/game
-java -jar -Xmx2G -Xms2G -jar server.jar &
+$JAVA_CMD -jar -Xmx2G -Xms2G -jar server.jar &
 SERVER_PID=$!
 
 lsof -i:43595 > /dev/null
@@ -35,8 +41,7 @@ while [ $? -eq "1" ]; do
 done;
 
 sleep 2
-java -Xmx1G -Xms1G -jar client.jar
+$JAVA_CMD -Xmx1G -Xms1G -jar client.jar
 
 kill $SERVER_PID
 exit
-
